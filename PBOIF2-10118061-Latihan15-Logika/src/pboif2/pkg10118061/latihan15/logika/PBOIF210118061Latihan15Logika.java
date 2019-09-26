def568f9ff9a0cd887b2805e57936b5ecc139993
Iqbal-Argiansyah <55845460+Iqbal-Argiansyah@users.noreply.github.com>
2019-09-26 22:21:21 +0700

    Add files via upload

---
+++ PBOIF2-10118061-Latihan15-Logika/src/pboif2/pkg10118061/latihan15/logika/PBOIF210118061Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118061.latihan15.logika;
+
+/**
+ *
+ * @author Iqbal
+ *  NAMA      : Iqbal Argiansyah
+ * KELAS     : PBOIF2
+ * NIM       : 10118061
+ * Deskripsi : Logika
+ */
+public class PBOIF210118061Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
