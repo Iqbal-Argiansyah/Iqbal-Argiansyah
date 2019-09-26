59ec194eff7778c74c402e7418b126e9d3dbeecd
Iqbal-Argiansyah <55845460+Iqbal-Argiansyah@users.noreply.github.com>
2019-09-26 21:59:42 +0700

    Add files via upload

---
+++ PBOIF2-10118061-Latihan1-Biodata/src/pboif2/pkg10118061/latihan1/biodata/PBOIF210118061Latihan1Biodata.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118061.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA      : Iqbal Argiansyah
+ * KELAS     : PBOIF2
+ * NIM       : 10118061
+ * Deskripsi : Menampilkan value ke layar.
+ */
+public class PBOIF210118061Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR");
+        System.out.println("-----------------------------");
+        System.out.println("NAMA          : Iqbal Argiansyah");
+        System.out.println("ALAMAT        : Jl Dipatiukur ");
+        System.out.println("TEMPAT LAHIR  : Bangka Belitung");
+        System.out.println("TANGGAL LAHIR : 25 September 2000");
+        System.out.println("-----------------------------");
+        System.out.println("HOBI          : Sepak Bola ");
+        System.out.println("MAKES         : Nasi ");
+        System.out.println("MIKES         : Minuman Dingin ");
+        
+        
+        
+                
+        
+    }
+    
+}
